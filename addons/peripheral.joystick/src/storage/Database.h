49ec1c0e352964bd4c7a10407f1f1284dd7104f0
Garrett Brown <garbearucla@gmail.com>
2015-01-18 01:42:51 -0800

    Persistent storage work-in-progress

--- addons/peripheral.joystick/src/storage/Database.h
+++ addons/peripheral.joystick/src/storage/Database.h
@@ -19,11 +19,47 @@
  */
 #pragma once
 
+#include "xbmc_peripheral_utils.hpp"
+
+#include <map>
+#include <stdint.h>
+#include <string>
+
+namespace ADDON
+{
+  // TODO
+  typedef std::map<JOYSTICK_ID, std::string> ButtonLabels;
+  typedef std::map<JOYSTICK_ID, std::string> ButtonColors;
+  typedef std::map<JOYSTICK_ID, std::string> ButtonIcons;
+}
+
 namespace JOYSTICK
 {
-  class IDatabase
+  class IPersistentJoystick
   {
   public:
-    virtual ~IDatabase(void) { }
+    virtual ~IPersistentJoystick(void) { }
+
+    virtual std::string Name(void) = 0;
+    virtual std::string Provider(void) = 0;
+    virtual uint16_t    VendorID(void) = 0;
+    virtual uint16_t    ProductID(void) = 0;
+    virtual std::string Icon(void) = 0;
+    virtual std::string Diagram(void) = 0;
+    virtual void        GetButtonMap(ADDON::ButtonMap& buttonMap) = 0;
+    virtual void        GetButtonLabels(ADDON::ButtonLabels& labels) = 0;
+    virtual void        GetButtonColors(ADDON::ButtonColors& colors) = 0;
+    virtual void        GetButtonIcons(ADDON::ButtonIcons& icons) = 0;
+
+    virtual void SetName(const std::string& name) = 0;
+    virtual void SetProvider(const std::string& provider) = 0;
+    virtual void SetVendorID(uint16_t vendorId) = 0;
+    virtual void SetProductID(uint16_t productId) = 0;
+    virtual void SetIcon(const std::string& name) = 0;
+    virtual void SetDiagram(const std::string& name) = 0;
+    virtual void SetButtonMap(const ADDON::ButtonMap& buttonMap) = 0;
+    virtual void SetButtonLabels(const ADDON::ButtonLabels& labels) = 0;
+    virtual void SetButtonColors(const ADDON::ButtonColors& colors) = 0;
+    virtual void SetButtonIcons(const ADDON::ButtonIcons& icons) = 0;
   };
 }
